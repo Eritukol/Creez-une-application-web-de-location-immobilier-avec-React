335c514d59d73b9b21fc71236931c144379509fa
Ulrich <h.ulrich78160@gmail.com>
2025-06-18 18:36:03 +0200

    ajustement scss logement et rajout des étoiles

--- src/pages/Logement.jsx
+++ src/pages/Logement.jsx
@@ -1,59 +1,59 @@
-// Importations nécessaires
 import { useParams, Navigate } from 'react-router-dom'
 import logements from '../data/logements.json'
-import '../styles/logements.scss' // À créer plus tard pour le style
+import '../styles/logements.scss'
 
 export default function Logement() {
-  // Récupère l'ID depuis l'URL (ex: /logement/c67ab8a7)
   const { id } = useParams()
-
-  // Recherche du logement dans les données
   const logement = logements.find((item) => item.id === id)
 
-  // Si aucun logement trouvé → redirection vers la page 404
   if (!logement) {
     return <Navigate to="/404" />
   }
 
-  // Déstructure les infos du logement pour plus de lisibilité
   const { title, location, description, tags, equipments, host, rating, pictures } = logement
 
   return (
     <div className="logement">
-      <div className="logement__gallery">
-        {/* Affiche la première image du logement */}
-        <img src={pictures[0]} alt={title} />
-      </div>
+      <img src={pictures[0]} alt={title} className="logement__banner" />
 
-      <div className="logement__infos">
+      <div className="logement__header">
         <div>
-          <h1>{title}</h1>
-          <p>{location}</p>
+          <h1 className="logement__title">{title}</h1>
+          <p className="logement__location">{location}</p>
 
-          {/* Liste des tags */}
           <div className="logement__tags">
             {tags.map((tag, index) => (
-              <span key={index} className="logement__tag">
+              <span key={index} className="logement__tags-item">
                 {tag}
               </span>
             ))}
           </div>
         </div>
 
-        {/* Infos de l’hôte */}
-        <div className="logement__host">
-          <p>{host.name}</p>
-          <img src={host.picture} alt={host.name} />
+        <div>
+          <div className="logement__host">
+            <p className="logement__host-name">{host.name}</p>
+            <img src={host.picture} alt={host.name} className="logement__host-picture" />
+          </div>
+
+          <div className="logement__rating">
+            {[1, 2, 3, 4, 5].map((i) => (
+              <img
+                key={i}
+                src={i <= rating ? '/star-full.png' : '/star-empty.png'}
+                alt={i <= rating ? 'étoile pleine' : 'étoile vide'}
+                className="star"
+              />
+            ))}
+          </div>
         </div>
       </div>
 
-      {/* Description */}
       <div className="logement__description">
         <h2>Description</h2>
         <p>{description}</p>
       </div>
 
-      {/* Équipements */}
       <div className="logement__equipments">
         <h2>Équipements</h2>
         <ul>
