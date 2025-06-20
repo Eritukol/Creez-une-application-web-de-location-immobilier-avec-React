165db0e3f3e6cd02a76c140183f4f485ea9227bb
Ulrich <h.ulrich78160@gmail.com>
2025-06-20 09:02:29 +0200

    rajout des infos dans la page about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,39 @@
-// Import de React
-import React from 'react';
+import Collapse from "../components/Collapse";
+import "../styles/about.scss";
 
-// Composant de la page d'accueil
-function Home() {
+export default function About() {
   return (
-    <div>
-      <h1>Bienvenue sur la page A propos</h1>
+    <div className="about">
+      <img
+        src="/banner-about.png"
+        alt="Paysage"
+        className="about__banner"
+      />
+
+      <div className="about__collapses">
+        <Collapse title="Fiabilité">
+          <p> Les annonces postées sur Kasa garantissent une fiabilité totale. Les
+          photos sont conformes aux logements, et toutes les informations sont
+          régulièrement vérifiées par nos équipes.</p>
+        </Collapse>
+        <Collapse title="Respect">
+          <p> La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+          comportement discriminatoire ou de perturbation du voisinage entraînera
+          une exclusion de notre plateforme.</p>
+        </Collapse>
+        <Collapse title="Service">
+          <p>Nos équipes se tiennent à votre disposition pour vous fournir une
+          expérience parfaite. N'hésitez pas à nous contacter si vous avez la
+          moindre question.</p>
+        </Collapse>
+        <Collapse title="Sécurité">
+          <p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
+          pour les voyageurs, chaque logement correspond aux critères de sécurité
+          établis par nos services. En laissant une note aussi bien à l'hôte
+          qu'au locataire, cela permet à nos équipes de vérifier que les standards
+          sont bien respectés.</p>
+        </Collapse>
+      </div>
     </div>
   );
 }
-
-export default Home;
