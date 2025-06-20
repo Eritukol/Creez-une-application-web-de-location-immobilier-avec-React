cb1e91a252b38b1ac05023dc8d1f0d8289deab43
Ulrich <h.ulrich78160@gmail.com>
2025-06-20 08:49:35 +0200

    ajustement page 404

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,13 +1,14 @@
-// Import de React
-import React from 'react';
+import { Link } from "react-router-dom";
+import "../styles/error.scss";
 
-// Composant de la page d'accueil
-function Home() {
+export default function Error404() {
   return (
-    <div>
-      <h1>Error 404</h1>
+    <div className="error">
+      <h1 className="error__code">404</h1>
+      <p className="error__message">Oups ! La page que vous demandez n'existe pas.</p>
+      <Link to="/" className="error__link">
+        Retourner sur la page d’accueil
+      </Link>
     </div>
   );
 }
-
-export default Home;
